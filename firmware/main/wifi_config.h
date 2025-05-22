3a5b8d91c919b485928a78c6079c04a264640334
halimeturkmnen <turkmnhalime842@gmail.com>
2025-05-22 12:57:11 +0300

    Initial commit with firmware, Node-RED flows, Firestore structure and Flutter app

---
+++ firmware/main/wifi_config.h
@@ -0,0 +1,9 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+//#define WIFI_SSID "SUPERONLINE_WiFi_71B0"
+//#define WIFI_PASS "4AALKLWJKYY7"
+#define WIFI_SSID "HalimeiPhone"
+#define WIFI_PASS "halime.1234"
+
+#endif /* WIFI_CONFIG_H */
